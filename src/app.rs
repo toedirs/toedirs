ab41e8192a2638f180c4eaea00eb7a4063fda616
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2023-08-26 19:41:04 +0200

    add tailwind and overview view

--- src/app.rs
+++ src/app.rs
@@ -16,7 +16,7 @@ pub fn App(cx: Scope) -> impl IntoView {
         <Stylesheet id="leptos" href="/pkg/toedirs.css"/>
 
         // sets the document title
-        <Title text="Welcome to Leptos"/>
+        <Title text="Welcome to Toedi"/>
 
         // content for this welcome page
         <Router fallback=|cx| {
@@ -27,24 +27,57 @@ pub fn App(cx: Scope) -> impl IntoView {
             }
             .into_view(cx)
         }>
+            <nav class="bg-gray-800">
+                <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                    <div class="relative flex h-16 items-center justify-between">
+                        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                            <div class="flex flex-shrink-0 items-center text-white">
+                                Toedi
+                            </div>
+                            <div class="hidden sm:ml-6 sm:block">
+                                <div class="flex space-x-4">
+                                    <A href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Overview</A>
+                                    <A href="/activities" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Activities</A>
+                                </div>
+                            </div>
+
+                            <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                                <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                    <span class="absolute -inset-1.5">    </span>
+                                    <span class="sr-only">Upload</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                                    </svg>
+                                </button>
+                            </div>                        
+                        </div>
+                    </div>
+                </div>
+            </nav>
             <main>
                 <Routes>
-                    <Route path="" view=|cx| view! { cx, <HomePage/> }/>
+                    <Route path="" view=|cx| view! { cx, <Overview/> }/>
                 </Routes>
             </main>
         </Router>
     }
 }
 
-/// Renders the home page of your application.
 #[component]
-fn HomePage(cx: Scope) -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(cx, 0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
-    view! { cx,
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+fn Overview(cx: Scope) -> impl IntoView {
+    //overview page
+    view! {cx,
+        <div class="grid grid-cols-3 gap-4">
+            <div class="block bg-gray-400 h-64 rounded">
+                Pie Chart
+            </div>
+            <div class="block bg-gray-400 h-64 rounded">
+                Training LoadChart
+            </div>
+            <div class="block bg-gray-400 h-64 rounded">
+                Fitness & Fatigue
+            </div>
+        </div>   
     }
 }
+
