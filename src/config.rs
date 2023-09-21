c34f024c5c435b6b70438afade174ab63f5b0b85
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2023-09-21 17:31:54 +0200

    add sqlx and global config/connection pool

--- src/config.rs
+++ src/config.rs
@@ -1,37 +1,41 @@
-use figment::{
-    providers::{Env, Format, Toml},
-    Figment,
-};
-use once_cell::sync::OnceCell;
-use serde::Deserialize;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "ssr")]{
+        use figment::{
+            providers::{Env, Format, Toml},
+            Figment,
+        };
+        use once_cell::sync::OnceCell;
+        use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
-pub struct Database {
-    pub url: String,
-    pub port: Option<u8>,
-    pub user: Option<String>,
-    pub password: Option<String>,
-    pub database: Option<String>,
-}
+        #[derive(Deserialize, Debug)]
+        pub struct Database {
+            pub host: String,
+            pub port: u16,
+            pub user: String,
+            pub password: String,
+            pub database: String,
+        }
 
-#[derive(Deserialize, Debug)]
-pub struct Config {
-    pub db: Database,
-}
+        #[derive(Deserialize, Debug)]
+        pub struct Config {
+            pub db: Database,
+        }
 
-static INSTANCE: OnceCell<Config> = OnceCell::new();
+        static INSTANCE: OnceCell<Config> = OnceCell::new();
 
-impl Config {
-    pub fn global() -> &'static Config {
-        INSTANCE.get().expect("configuration is not initialized")
-    }
+        impl Config {
+            pub fn global() -> &'static Config {
+                INSTANCE.get().expect("configuration is not initialized")
+            }
 
-    pub fn load() -> () {
-        let config: Config = Figment::new()
-            .merge(Toml::file("toedi.toml"))
-            .merge(Env::prefixed("TOEDI_"))
-            .extract()
-            .expect("cannot load config");
-        INSTANCE.set(config).expect("config only set once");
+            pub fn load() -> () {
+                let config: Config = Figment::new()
+                    .merge(Toml::file("toedi.toml"))
+                    .merge(Env::prefixed("TOEDI_"))
+                    .extract()
+                    .expect("cannot load config");
+                INSTANCE.set(config).expect("config only set once");
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,66 @@
 mod fit_upload;
 
-#[cfg(feature = "ssr")]
-use sqlx::postgres::{PgPoolOptions, Pool, PgPool};
-#[cfg(feature = "ssr")]
-static CONNECTION_POOL: OnceCell<Pool<PgPool> = OnceCell::new();
+cfg_if::cfg_if! {
+    if #[cfg(feature = "ssr")]{
+        use axum::{routing::post, Router};
+        use leptos::logging::log;
+        use leptos::*;
+        use leptos_axum::{generate_route_list, LeptosRoutes};
+        use toedirs::app::*;
+        use toedirs::config::Config;
+        use toedirs::fileserv::file_and_error_handler;
+        use once_cell::sync::OnceCell;
+        use sqlx::{Pool, Postgres,{postgres::{PgPoolOptions, PgPool}}};
 
-#[cfg(feature = "ssr")]
-#[tokio::main]
-async fn main() {
-    use axum::{routing::post, Router};
-    use leptos::logging::log;
-    use leptos::*;
-    use leptos_axum::{generate_route_list, LeptosRoutes};
-    use toedirs::app::*;
-    use toedirs::config::Config;
-    use toedirs::fileserv::file_and_error_handler;
-    use once_cell::sync::OnceCell;
-    
+        static CONNECTION_POOL: OnceCell<ConnectionPool> = OnceCell::new();
+        #[derive(Debug)]
+        struct ConnectionPool(Pool<Postgres>);
 
-    Config::load();
-    
-    let pool = PgPoolOptions::new().max_connections(50).connect(format!("postgresql://{credentials}{host}/{database}", ))
-    CONNECTION_POOL.set()
+        #[tokio::main]
+        async fn main() {
+            Config::load();
+            let config = Config::global();
 
-    simple_logger::init_with_level(log::Level::Info).expect("couldn't initialize logging");
+            let pool = PgPoolOptions::new().max_connections(50).connect(format!("postgresql://{user}:{password}@{host}:{port}/{database}", user=config.db.user, password=config.db.password, host=config.db.host, port=config.db.port, database=config.db.database).as_str()).await.expect("couldn't connect to database");
+            CONNECTION_POOL.set(ConnectionPool(pool)).expect("failed to set config");
 
-    // Setting get_configuration(None) means we'll be using cargo-leptos's env values
-    // For deployment these variables are:
-    // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
-    // Alternately a file can be specified such as Some("Cargo.toml")
-    // The file would need to be included with the executable when moved t:wao deployment
-    let conf = get_configuration(None).await.unwrap();
-    let leptos_options = conf.leptos_options;
-    let addr = leptos_options.site_addr;
-    let routes = generate_route_list(|| view! { <App/> }).await;
+            simple_logger::init_with_level(log::Level::Info).expect("couldn't initialize logging");
 
-    // build our application with a route
-    let app = Router::new()
-        .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
-        .route("/api/upload_fit_file", post(fit_upload::upload_fit_file))
-        .leptos_routes(&leptos_options, routes, || view! { <App/> })
-        .fallback(file_and_error_handler)
-        .with_state(leptos_options);
+            // Setting get_configuration(None) means we'll be using cargo-leptos's env values
+            // For deployment these variables are:
+            // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
+            // Alternately a file can be specified such as Some("Cargo.toml")
+            // The file would need to be included with the executable when moved t:wao deployment
+            let conf = get_configuration(None).await.unwrap();
+            let leptos_options = conf.leptos_options;
+            let addr = leptos_options.site_addr;
+            let routes = generate_route_list(|| view! { <App/> }).await;
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
-    log!("listening on http://{}", &addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
+            // build our application with a route
+            let app = Router::new()
+                .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
+                .route("/api/upload_fit_file", post(fit_upload::upload_fit_file))
+                .leptos_routes(&leptos_options, routes, || view! { <App/> })
+                .fallback(file_and_error_handler)
+                .with_state(leptos_options);
 
-impl Pool<PgPool> { 
-    pub fn global() -> &'static Pool<PgPool> {
-        CONNECTION_POOL.get().expect("connection pool not initialized")
+            // run our app with hyper
+            // `axum::Server` is a re-export of `hyper::Server`
+            log!("listening on http://{}", &addr);
+            axum::Server::bind(&addr)
+                .serve(app.into_make_service())
+                .await
+                .unwrap();
+        }
+
+        impl ConnectionPool {
+            pub fn global() -> &'static ConnectionPool{
+                CONNECTION_POOL.get().expect("connection pool not initialized")
+            }
+        }
     }
 }
-
+#[cfg(feature = "ssr")]
 #[cfg(not(feature = "ssr"))]
 pub fn main() {
     // no client-side main function
