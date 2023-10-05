136169ed494da6ecb681225df4d8e6a0d5c68607
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2023-10-05 19:00:56 +0200

    add color scheme to barcode

--- leptos_charts/src/lib.rs
+++ leptos_charts/src/lib.rs
@@ -1,8 +1,23 @@
 use leptos::*;
 
+const CATPPUCCIN_COLORS: &[&str] = &[
+    "#dc8a78", //rosewater
+    "#8839ef", //Mauve
+    "#fe640b", //Peach
+    "#04a5e5", //Sky
+    "#ea76cb", //Pink
+    "#1e66f5", //Blue
+    "#d20f39", //Red
+    "#df8e1d", //yellow
+    "#209fb5", //Sapphire
+    "#7287fd", //lavender
+    "#e64553", //maroon
+];
+
 #[component]
 pub fn BarChart(
     values: ReadSignal<Vec<(usize, f64)>>,
+    // colors: Option<&'chart [&'chart str]>,
     #[prop(attrs)] attrs: Vec<(&'static str, Attribute)>,
 ) -> impl IntoView {
     let num_bars = create_memo(move |_| values.get().len() as f64);
@@ -14,16 +29,25 @@ pub fn BarChart(
             .map(|(_i, v)| v.clone())
             .unwrap()
     });
+    let values = create_memo(move |_| {
+        values
+            .get()
+            .into_iter()
+            .map(|(i, v)| (i, v))
+            .zip(CATPPUCCIN_COLORS.into_iter().cycle())
+            .collect::<Vec<((usize, f64), &&str)>>()
+    });
 
     view! {
         <svg viewBox="0 0 100 100" {..attrs}>
             <g transform="matrix(1 0 0 -1 0 100)">
-                {move || values.get().into_iter().map(|(i, v)|view!{
+                {move || values.get().into_iter().map(|((i, v), color)|view!{
                     <rect
                         x=move || (100.0 / num_bars.get() * i as f64)
                         y=0
                         width=move || (80.0 / num_bars.get())
                         height=move || (100.0 * v / max.get())
+                        style=format!("fill:{}", color)
                     />
                 }).collect_view()}
             </g>
