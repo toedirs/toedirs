de5f536a7bec85c81a945ab4a6ff1bf95dbf79e6
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2024-01-25 17:17:09 +0100

    add workout parameters to DB

--- migrations/20240117191147_add_workout_instance.down.sql
+++ migrations/20240117191147_add_workout_instance.down.sql
@@ -1,3 +1,9 @@
 -- Add down migration script here
+DROP TABLE IF EXISTS parameter_link;
+
+DROP TABLE IF EXISTS workout_parameter;
+
 DROP TABLE IF EXISTS workout_instances;
+
+DROP TYPE IF EXISTS workout_parameter_type;
 
--- migrations/20240117191147_add_workout_instance.up.sql
+++ migrations/20240117191147_add_workout_instance.up.sql
@@ -8,3 +8,24 @@ CREATE TABLE IF NOT EXISTS workout_instances (
     active boolean NOT NULL DEFAULT TRUE
 );
 
+CREATE TYPE workout_parameter_type AS ENUM (
+    'time_s',
+    'distance_m',
+    'trainingload'
+);
+
+CREATE TABLE IF NOT EXISTS workout_parameter (
+    id bigserial NOT NULL PRIMARY KEY,
+    workout_template_id integer NOT NULL REFERENCES workout_templates (id) ON DELETE CASCADE,
+    name text NOT NULL,
+    parameter_type workout_parameter_type NOT NULL,
+    value integer NOT NULL,
+    scaling boolean NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE IF NOT EXISTS parameter_link (
+    instance_id integer NOT NULL REFERENCES workout_instances (id) ON DELETE CASCADE,
+    parameter_id integer NOT NULL REFERENCES workout_parameter (id) ON DELETE CASCADE,
+    value_override integer
+);
+
